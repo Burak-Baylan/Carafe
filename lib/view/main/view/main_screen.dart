45b0f8002e19f158bfad4fac5f95a1aea51183bc
Burak-Baylan <dsjkadnas@gmail.com>
2021-12-09 14:13:57 +0300

    add: main view

---
+++ lib/view/main/view/main_screen.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+
+import '../../../app/constants/app_constants.dart';
+import '../../../core/base/view/base_view.dart';
+import '../../../core/extensions/context_extensions.dart';
+import '../../../core/extensions/int_extensions.dart';
+import '../../../core/init/navigation/service/navigation_service.dart';
+import '../view_model/main_view_view_model.dart';
+import 'add_post/view/add_post_page.dart';
+import 'home/view/home_view.dart';
+import 'sub_views/bottom_navigation/bottom_navigation.dart';
+import 'sub_views/drawer/main_screen_drawer.dart';
+
+final MainViewViewModel mainVm = MainViewViewModel();
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends BaseView<MainScreen> {
+  @override
+  late BuildContext context;
+
+  @override
+  Widget build(BuildContext context) {
+    this.context = context;
+    _setViewModel(context);
+    return Scaffold(
+      backgroundColor: AppColors.backGroundGrey,
+      drawer: MainScreenDrawer(viewModel: mainVm),
+      floatingActionButton: _fab,
+      bottomNavigationBar: MainBottomNavigation(viewModel: mainVm),
+      body: SafeArea(child: _body),
+    );
+  }
+
+  Widget get _fab => Observer(
+        builder: (_) => AnimatedContainer(
+          width: mainVm.isFabVisible ? context.height / 13 : 0,
+          height: mainVm.isFabVisible ? context.height / 13 : 0,
+          duration: 100.durationMilliseconds,
+          child: _fabWidget,
+        ),
+      );
+
+  Widget get _fabWidget => FloatingActionButton(
+        backgroundColor: context.colorScheme.secondaryVariant,
+        child: Icon(
+          Icons.add_outlined,
+          color: Colors.white,
+          size: mainVm.isFabVisible ? context.height / 25 : 0,
+        ),
+        onPressed: () =>
+            NavigationService.instance.customNavigateToPage(AddPostPage()),
+      );
+
+  Widget get _body {
+    return NestedScrollView(
+      floatHeaderSlivers: true,
+      body: HomeView(),
+      headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) => [
+        _appBar,
+      ],
+    );
+  }
+
+  Widget get _appBar => SliverAppBar(
+        backgroundColor: AppColors.white,
+        centerTitle: true,
+        forceElevated: true,
+        floating: true,
+        snap: true,
+        elevation: Theme.of(context).appBarTheme.elevation,
+        leading: _leading,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(bottom: 15.radiusCircular),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.filter_alt_outlined,
+              color: context.theme.colorScheme.primary,
+            ),
+            onPressed: () {},
+          ),
+        ],
+        title: Text(
+          "Home",
+          style: TextStyle(
+            fontSize: 16,
+            color: context.theme.colorScheme.primary,
+          ),
+        ),
+      );
+
+  Widget get _leading => Padding(
+        padding: const EdgeInsets.all(10),
+        child: Builder(builder: (context) {
+          return GestureDetector(
+            onTap: () => context.openDrawer,
+            child: const CircleAvatar(
+              backgroundImage: NetworkImage(
+                'https://via.placeholder.com/140x100',
+                scale: 20,
+              ),
+            ),
+          );
+        }),
+      );
+
+  _setViewModel(BuildContext context) {
+    mainVm.setContext(context);
+    mainVm.userControl(user);
+  }
+}
---
+++ lib/view/main/view/sub_views/bottom_navigation/bottom_navigation.dart
@@ -0,0 +1,60 @@
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import '../../../../../core/extensions/context_extensions.dart';
+import '../../../../../core/widgets/border_container.dart';
+import '../../../view_model/main_view_view_model.dart';
+
+class MainBottomNavigation extends StatelessWidget {
+  MainBottomNavigation({
+    Key? key,
+    required this.viewModel,
+  }) : super(key: key);
+  final MainViewViewModel viewModel;
+
+  late BuildContext context;
+
+  @override
+  Widget build(BuildContext context) {
+    this.context = context;
+    return BorderContainer.all(
+      radius: 30,
+      child: Observer(builder: (_) {
+        return BottomNavyBar(
+          selectedIndex: viewModel.currentIndex,
+          items: <BottomNavyBarItem>[
+            _homeIcon,
+            _searchIcon,
+            _notificationsIcon,
+            _messagesIcon,
+          ],
+          onItemSelected: (index) => viewModel.changeIndex(index),
+        );
+      }),
+    );
+  }
+
+  BottomNavyBarItem get _homeIcon => BottomNavyBarItem(
+        icon: const Icon(Icons.home_outlined),
+        activeColor: context.theme.colorScheme.primary,
+        title: const Text('Home'),
+      );
+
+  BottomNavyBarItem get _searchIcon => BottomNavyBarItem(
+        icon: const Icon(Icons.search_outlined),
+        activeColor: context.theme.colorScheme.primary,
+        title: const Text('Search'),
+      );
+
+  BottomNavyBarItem get _notificationsIcon => BottomNavyBarItem(
+        icon: const Icon(Icons.notifications_outlined),
+        activeColor: context.theme.colorScheme.primary,
+        title: const Text('Notification'),
+      );
+
+  BottomNavyBarItem get _messagesIcon => BottomNavyBarItem(
+        icon: const Icon(Icons.mail_outline_outlined),
+        activeColor: context.theme.colorScheme.primary,
+        title: const Text('Messages'),
+      );
+}
