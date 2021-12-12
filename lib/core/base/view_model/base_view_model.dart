635b755e135550a08e9f71a8149db8653fc283bd
Burak-Baylan <dsjkadnas@gmail.com>
2021-12-12 19:33:27 +0300

    update: base_view_model, double_extension

--- lib/core/base/view_model/base_view_model.dart
+++ lib/core/base/view_model/base_view_model.dart
@@ -1,8 +1,13 @@
+import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
-
+import 'package:uuid/uuid.dart';
 import '../../firebase/auth/authentication/service/firebase_auth_service.dart';
-import '../../firebase/firestore/service/firebase_user_service.dart';
+import '../../firebase/firestore/manager/firebase_user_manager.dart';
+import '../../firebase/firestore/service/firebase.dart';
+import '../../firebase/storage/service/firebase_storage_service.dart';
 import '../../helpers/colorful_print.dart';
+import '../../helpers/dominant_color_getter.dart';
 import '../../init/navigation/service/navigation_service.dart';
 import '../../widgets/custom_alert_dialog.dart';
 
@@ -10,14 +15,22 @@ abstract class BaseViewModel {
   BuildContext? context;
   setContext(BuildContext context);
   FirebaseAuthService authService = FirebaseAuthService.instance;
-  FirebaseUserService userService = FirebaseUserService.instance;
+  FirebaseUserManager userService = FirebaseUserManager.instance;
 
-  navigateToPage({required String path, required Object? data}) {
-    NavigationService.instance.navigateToPage(
-      path: path,
-      data: data,
-    );
-  }
+  FirestoreService firestoreService = FirestoreService.instance;
+  FirebaseStorageService storageService = FirebaseStorageService.instance;
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  Timestamp currentTime = Timestamp.now();
+
+  Future<Color?> getDominantColor(File image) async =>
+      await DominantColorGetter.getColor(image);
+
+  navigateToPage({required String path, required Object? data}) =>
+      NavigationService.instance.navigateToPage(path: path, data: data);
+
+  replacePage({required String path, required Object? data}) =>
+      NavigationService.instance.replacePageNamed(path: path, data: data);
 
   showAlert(
     String title,
@@ -42,38 +55,11 @@ abstract class BaseViewModel {
         negativeButtonText: negativeButtonText ?? "Confirm",
       ).show();
 
-  showBottomSheetWith2Action(
-    BuildContext context, {
-    required String actionOneText,
-    required String actionTwoText,
-    required Function() actionOnePressed,
-    required Function() actionTwoPressed,
-  }) =>
-      showCupertinoModalPopup(
-        context: context,
-        builder: (context) => CupertinoActionSheet(
-          actions: [
-            CupertinoActionSheetAction(
-              onPressed: () {
-                actionOnePressed();
-                Navigator.of(context, rootNavigator: true).pop("Discard");
-              },
-              child: Text(actionOneText),
-            ),
-            CupertinoActionSheetAction(
-              onPressed: () {
-                actionTwoPressed();
-                Navigator.of(context, rootNavigator: true).pop("Discard");
-              },
-              child: Text(actionTwoText),
-            )
-          ],
-        ),
-      );
-
   printYellow(String text) => ColorfulPrint.yellow(text);
 
   printRed(String text) => ColorfulPrint.red(text);
 
   printGreen(String text) => ColorfulPrint.green(text);
+
+  get getRandomId => const Uuid().v4();
 }
--- lib/core/extensions/double_extensions.dart
+++ lib/core/extensions/double_extensions.dart
@@ -8,3 +8,9 @@ extension EdgeInsetsExtensions on double{
   EdgeInsets get edgeIntesetsOnlyLeft => EdgeInsets.only(left: this);
   EdgeInsets get edgeIntesetsTopRight => EdgeInsets.only(top: this, right: this);
 }
+
+extension SizedBoxExtension on double {
+  SizedBox get sizedBoxOnlyHeight => SizedBox(height: toDouble());
+  SizedBox get sizedBoxOnlyWidth => SizedBox(width: toDouble());
+  SizedBox get sizedBox => SizedBox(width: toDouble(), height: toDouble());
+}
