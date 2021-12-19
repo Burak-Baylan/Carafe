52472c7438b56a373ced23bb1b037ffdfee56f65
Burak-Baylan <dsjkadnas@gmail.com>
2021-12-20 00:57:38 +0300

    add: firebase_constants

---
+++ lib/core/constants/firebase/firebase_constants.dart
@@ -0,0 +1,58 @@
+import 'package:Carafe/core/firebase/base/firebase_base.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirebaseConstants extends FirebaseBase {
+  static FirebaseConstants? _instance;
+  static FirebaseConstants get instance =>
+      _instance = _instance == null ? FirebaseConstants._init() : _instance!;
+  FirebaseConstants._init();
+
+  //* Post paths
+  String postsText = "posts";
+  String commentCountText = "comment_count";
+  String createdAtText = "created_at";
+  String imageLinksText = "image_links";
+  String imageDominantColorsText = "image_dominant_color";
+  String likeCountText = "like_count";
+  String postIdText = "postId";
+  String postNotificationsText = "post_notifications";
+  String textText = "text";
+  String authorIdText = "author_id";
+  //*
+
+  String collectionLikesText = "likers";
+  String postLikersText = "likes";
+  String usersText = "Users";
+
+  @override
+  DocumentReference<Map<String, dynamic>> userDocRef(String userId) =>
+      allUsersCollectionRef.doc(userId);
+  @override
+  DocumentReference<Map<String, dynamic>> postDocRef(String postId) =>
+      allPostsCollectionRef.doc(postId);
+
+  CollectionReference<Map<String, dynamic>> postLikesCollectionRef(
+    String postId
+  ) =>
+      allPostsCollectionRef.doc(postId).collection(postLikersText);
+
+  @override
+  CollectionReference<Map<String, dynamic>> get allUsersCollectionRef =>
+      firestore.collection(usersText);
+  @override
+  CollectionReference<Map<String, dynamic>> get allPostsCollectionRef =>
+      firestore.collection(postsText);
+
+  Query<Map<String, dynamic>> get postsCreatedDescending =>
+      allPostsCollectionRef.orderBy(createdAtText, descending: true);
+
+  Query<Map<String, dynamic>> get postsCreatedAscending =>
+      allPostsCollectionRef.orderBy(createdAtText, descending: false);
+
+  Query<Map<String, dynamic>> userLikeStatusPath(
+    String postId,
+    String userId,
+  ) =>
+      postLikesCollectionRef(postId)
+          .where(authorIdText, isEqualTo: userId);
+}
