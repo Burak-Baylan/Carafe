34ced85fca6d07a7ab287017e8d56d65cd3e37a5
Burak-Baylan <dsjkadnas@gmail.com>
2021-12-21 16:18:43 +0300

    update: dominant_color class name changed to image_color_getter and add some new features

---
+++ lib/core/helpers/image_colors_getter.dart
@@ -0,0 +1,49 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:palette_generator/palette_generator.dart';
+
+class ImageColorsGetter {
+  Future<Color> findSuitableColor(File image) async {
+    Color? color;
+    color = await darkMutedColor(image);
+    if (color != null) return color;
+    color = await darkVibrantColor(image);
+    if (color != null) return color;
+    color = await lightVibrantColor(image);
+    if (color != null) return color;
+    color = await lightMutedColor(image);
+    if (color != null) return color;
+    color = await dominantColor(image);
+    if (color != null) return color;
+    return Colors.black;
+  }
+
+  Future<Color?> darkMutedColor(File image) async {
+    var palette = await getPalette(image);
+    return palette.darkMutedColor?.color;
+  }
+
+  Future<Color?> darkVibrantColor(File image) async {
+    var palette = await getPalette(image);
+    return palette.darkVibrantColor?.color;
+  }
+
+  Future<Color?> lightMutedColor(File image) async {
+    var palette = await getPalette(image);
+    return palette.lightMutedColor?.color;
+  }
+
+  Future<Color?> lightVibrantColor(File image) async {
+    var palette = await getPalette(image);
+    return palette.lightVibrantColor?.color;
+  }
+
+  Future<Color?> dominantColor(File image) async {
+    var palette = await getPalette(image);
+    return palette.dominantColor?.color;
+  }
+
+  Future<PaletteGenerator> getPalette(File image) async =>
+      await PaletteGenerator.fromImageProvider(FileImage(image));
+}
