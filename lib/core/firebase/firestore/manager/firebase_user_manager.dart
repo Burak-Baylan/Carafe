8e588a8c8e62a7738158bb5a8b05042f23219ed7
Burak-Baylan <dsjkadnas@gmail.com>
2021-12-26 21:25:19 +0300

    update: user_manager(pinnde post & pinned post id)

--- lib/core/firebase/firestore/manager/firebase_user_manager.dart
+++ lib/core/firebase/firestore/manager/firebase_user_manager.dart
@@ -1,6 +1,8 @@
-import 'package:Carafe/pages/authenticate/model/user_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import '../../../../pages/authenticate/model/user_model.dart';
+import '../../../../pages/main/model/post_model.dart';
+import '../../../data/custom_data.dart';
 import '../../../error/custom_error.dart';
 import '../../base/firebase_base.dart';
 
@@ -11,15 +13,11 @@ class FirebaseUserManager extends FirebaseBase {
   FirebaseUserManager._init();
 
   Future<CustomError> createUser(UserModel userModel) async {
-    try {
-      await firebaseService.addDocument(
-        firestore.collection(firebaseConstants.usersText).doc(userModel.userId),
-        userModel.toJson(),
-      );
-      return CustomError(null);
-    } on FirebaseException catch (e) {
-      return CustomError(e.message);
-    }
+    var customError = await firebaseService.addDocument(
+      firestore.collection(firebaseConstants.usersText).doc(userModel.userId),
+      userModel.toJson(),
+    );
+    return customError;
   }
 
   Future<CustomError> updateUser(UserModel userModel) async => firebaseService
@@ -37,5 +35,31 @@ class FirebaseUserManager extends FirebaseBase {
     } on FirebaseException catch (e) {
       return CustomError(e.message);
     }
+  }
+
+  Future<CustomData<String>> userPinnedPostId(String userId) async {
+    var customPostModelData = await userPinnedPost(userId);
+    if (customPostModelData.error != null) {
+      return CustomData(null, CustomError('Something went wrong'));
+    }
+    return CustomData(customPostModelData.data!.postId, null);
+  }
+
+  Future<CustomData<PostModel>> userPinnedPost(String userId) async {
+    var pinnedPostRef = firebaseConstants.userPinnedPostDocRef(userId);
+    var pinnedPostData = await firebaseService.getDocument(pinnedPostRef);
+    if (pinnedPostData.error != null) {
+      return CustomData(null, CustomError('Something went wrong'));
+    }
+    var postIdData = firebaseService.getAField<String>(
+        pinnedPostData.data!, firebaseConstants.postIdText);
+    if (postIdData.error != null) {
+      return CustomData(null, CustomError('Something went wrong'));
+    }
+    var rawPostData = await firebaseService.getDocument(
+        firebaseConstants.allPostsCollectionRef.doc(postIdData.data));
+    var postData = rawPostData.data!.data() as Map<String, dynamic>;
+    PostModel postModel = PostModel.fromJson(postData);
+    return CustomData(postModel, null);
   }
 }
