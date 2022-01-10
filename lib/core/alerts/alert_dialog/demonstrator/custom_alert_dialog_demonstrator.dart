63ca576966d5fdf9146e19c2b2088a14f5b84822
Burak-Baylan <dsjkadnas@gmail.com>
2022-01-10 23:54:52 +0300

    add: animated text widget

---
+++ lib/core/alerts/alert_dialog/demonstrator/custom_alert_dialog_demonstrator.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import '../custom_alert_dialog.dart';
+
+class CustomAlerDialogDemonstrator {
+  static CustomAlerDialogDemonstrator? _instance;
+  static CustomAlerDialogDemonstrator get instance => _instance =
+      _instance == null ? CustomAlerDialogDemonstrator._init() : _instance!;
+  CustomAlerDialogDemonstrator._init();
+
+  show(
+    String title,
+    String message, {
+    required BuildContext context,
+    bool disableNegativeButton = false,
+    bool disablePositiveButton = false,
+    String? positiveButtonText,
+    String? negativeButtonText = 'Confirm',
+    Function? onPressedPositiveButton,
+    Function? onPressedNegativeButton,
+    double? borderRadius,
+  }) {
+    CustomAlertDialog(
+      context: context,
+      title: title,
+      message: message,
+      disableNegativeButton: disableNegativeButton,
+      disablePositiveButton: disablePositiveButton,
+      positiveButtonText: positiveButtonText,
+      onPressedPositiveButton: onPressedPositiveButton,
+      onPressedNegativeButton: onPressedNegativeButton,
+      negativeButtonText: negativeButtonText,
+      borderRadius: borderRadius,
+    ).show();
+  }
+}
---
+++ lib/core/widgets/bottom_to_top_animated_text.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+import '../extensions/double_extensions.dart';
+import '../extensions/int_extensions.dart';
+
+class BottomToTopAnimatedText extends StatelessWidget {
+  BottomToTopAnimatedText({
+    Key? key,
+    required this.text,
+    this.animate = true,
+    this.fontSize = 12,
+    this.color,
+  }) : super(key: key);
+
+  String text;
+  bool animate;
+  double fontSize;
+  Color? color;
+
+  String get randomId => const Uuid().v4();
+
+  @override
+  Widget build(BuildContext context) => AnimatedSwitcher(
+        transitionBuilder: (child, animation) => animate
+            ? this.animation(child, animation)
+            : _buildText(text, fontSize: fontSize, color: color),
+        duration: 200.durationMilliseconds,
+        child: _buildText(text, fontSize: fontSize, color: color),
+      );
+
+  Widget _buildText(String text, {double fontSize = 12, Color? color}) => Text(
+        text,
+        key: ValueKey<String>(randomId),
+        style: TextStyle(color: color ?? Colors.grey[700], fontSize: fontSize),
+      );
+
+  Widget animation(Widget child, Animation<double> animation) =>
+      SlideTransition(
+        child: child,
+        position: Tween<Offset>(begin: 1.0.offsetY, end: 0.0.offsetXY)
+            .animate(animation),
+      );
+}
