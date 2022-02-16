ba96899f50aa7f607d08d83bb422af7008c90da9
Burak-Baylan <dsjkadnas@gmail.com>
2022-02-16 16:27:14 +0300

    update: post image widget

--- lib/pages/main/view/sub_views/post_widget/post_widget/sub_widgets/post_image_widget/sub_views/post_image_widget.dart
+++ lib/pages/main/view/sub_views/post_widget/post_widget/sub_widgets/post_image_widget/sub_views/post_image_widget.dart
@@ -1,59 +1,68 @@
-import 'package:Carafe/app/constants/app_constants.dart';
-import 'package:Carafe/core/extensions/int_extensions.dart';
-import 'package:Carafe/core/widgets/border_container.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import '../../../../../../../../../app/constants/app_constants.dart';
+import '../../../../../../../../../core/extensions/int_extensions.dart';
+import '../../../../../../../../../core/helpers/random_id.dart';
+import '../../../../../../../../../core/widgets/border_container.dart';
 
 class PostImageWidget extends StatelessWidget {
-  PostImageWidget(
-      {Key? key,
-      required this.imageLink,
-      required this.borderRadius,
-      this.width = double.infinity,
-      required this.height,
-      required this.onPressedImage,
-      required this.getProvider})
-      : super(key: key);
+  PostImageWidget({
+    Key? key,
+    required this.imageLink,
+    required this.borderRadius,
+    this.width = double.infinity,
+    required this.height,
+    required this.onPressedImage,
+    required this.getProvider,
+  }) : super(key: key);
 
   String imageLink;
   BorderRadiusGeometry borderRadius;
   double width;
   double height;
-  Function() onPressedImage;
+  Function(String imageTag) onPressedImage;
   Function(ImageProvider<Object>? p) getProvider;
 
   @override
   Widget build(BuildContext context) => _image();
 
-  Widget _image() => InkWell(
-        onTap: () => onPressedImage(),
-        child: Hero(
-          tag: imageLink,
-          child: CachedNetworkImage(
-            fadeInDuration: 0.durationMilliseconds,
-            filterQuality: FilterQuality.medium,
-            placeholder: (context, url) => BorderContainer.all(
-              radius: 10,
-              color: AppColors.placeHolderGray,
-            ),
-            placeholderFadeInDuration: 100.durationMilliseconds,
-            imageBuilder: (context, provider) {
-              getProvider(provider);
-              return _buildImageContainer(provider);
-            },
-            imageUrl: imageLink,
-            fit: BoxFit.cover,
+  String tagForImage = getRandomId();
+
+  Widget _image() => Hero(
+        tag: tagForImage,
+        child: CachedNetworkImage(
+          fadeInDuration: 0.durationMilliseconds,
+          filterQuality: FilterQuality.medium,
+          placeholder: (context, url) => BorderContainer.all(
+            radius: 10,
+            color: AppColors.placeHolderGray,
           ),
+          placeholderFadeInDuration: 100.durationMilliseconds,
+          imageBuilder: (context, provider) {
+            getProvider(provider);
+            return _buildImageContainer(provider);
+          },
+          imageUrl: imageLink,
+          fit: BoxFit.cover,
         ),
       );
 
-  _buildImageContainer(ImageProvider<Object> provider) => Container(
-        width: width,
-        height: height,
-        decoration: BoxDecoration(
+  _buildImageContainer(ImageProvider<Object> provider) => Material(
+        shape: RoundedRectangleBorder(
           borderRadius: borderRadius,
-          border: Border.all(color: Colors.grey.shade500, width: 0.3),
-          image: DecorationImage(fit: BoxFit.cover, image: provider),
+          side: BorderSide(color: Colors.grey.shade500, width: 0.3),
+        ),
+        clipBehavior: Clip.antiAlias,
+        color: Colors.transparent,
+        child: Ink.image(
+          width: width,
+          height: height,
+          image: provider,
+          fit: BoxFit.cover,
+          child: InkWell(
+            onTap: () => onPressedImage(tagForImage),
+            splashColor: Colors.grey.shade100.withOpacity(.0),
+          ),
         ),
       );
 }
