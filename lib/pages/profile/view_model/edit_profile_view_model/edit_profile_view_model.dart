f8fa60bd23868e1f1bda02ef72c2dcb1cf98c10c
Burak-Baylan <dsjkadnas@gmail.com>
2022-02-21 00:12:01 +0300

    add: now users can edit their profiles

---
+++ lib/pages/profile/view_model/edit_profile_view_model/edit_profile_view_model.dart
@@ -0,0 +1,166 @@
+import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:intl/intl.dart';
+import 'package:mobx/mobx.dart';
+import '../../../../core/alerts/loading_alert_dialog.dart';
+import '../../../../core/base/view_model/base_view_model.dart';
+import '../../../../core/extensions/context_extensions.dart';
+import '../../../../core/extensions/string_extensions.dart';
+import '../../../../core/helpers/rounded_bottom_sheet.dart';
+import '../../../authenticate/model/user_model.dart';
+import '../profile_view_model/profile_view_model.dart';
+part 'edit_profile_view_model.g.dart';
+
+class EditProfileViewModel = _EditProfileViewModelBase
+    with _$EditProfileViewModel;
+
+abstract class _EditProfileViewModelBase extends BaseViewModel with Store {
+  @override
+  BuildContext? context;
+
+  @observable
+  File? ppImageFile;
+
+  @action
+  changePpImageFile(File? file) => ppImageFile = file;
+
+  @override
+  setContext(BuildContext context) {
+    this.context = context;
+    prepareCurrentValues();
+  }
+
+  late UserModel userModel;
+  late String oldDisplayName;
+  late String oldDescription;
+  late String oldWebsite;
+  late Timestamp? oldBirthDate;
+  late ProfileViewModel profileViewModel;
+  late TextEditingController displayNameController;
+  late TextEditingController descriptionController;
+  late TextEditingController websiteController;
+  late TextEditingController birthDateController;
+  FocusNode displayNameFocusNode = FocusNode();
+  FocusNode descriptionFocusNode = FocusNode();
+  FocusNode websiteFocusNode = FocusNode();
+  Timestamp? birthDate;
+  var loadingPBar = LoadingAlertDialog.instance;
+
+  void prepareCurrentValues() {
+    oldDisplayName = userModel.displayName;
+    oldDescription = userModel.profileDescription ?? '';
+    oldWebsite = userModel.website ?? '';
+    oldBirthDate = userModel.birthDate;
+  }
+
+  void setProfileViewModel(ProfileViewModel profileViewModel) {
+    this.profileViewModel = profileViewModel;
+    userModel = profileViewModel.userModel!;
+    displayNameController = TextEditingController(text: userModel.displayName);
+    descriptionController =
+        TextEditingController(text: userModel.profileDescription);
+    websiteController = TextEditingController(text: userModel.website);
+    birthDateController = TextEditingController(
+      text: profileViewModel.userModel!.birthDate != null
+          ? DateFormat("MMMM d, yyyy")
+              .format(profileViewModel.userModel!.birthDate!.toDate())
+          : '',
+    );
+  }
+
+  Future checkChanges() async {
+    context!.closeKeyboard;
+    startLoadingBar();
+    String newDisplayName = displayNameController.text;
+    String newDescription = descriptionController.text;
+    String newWebsite = websiteController.text;
+    Timestamp? newBirthDate = birthDate;
+
+    if (isProfilePhotoChanged()) {
+      var response = await userManager.updateUserProfilePhoto(ppImageFile!);
+      if (response) {
+        printGreen('pp changed');
+      }
+    }
+
+    if (isDisplayNameChanged()) {
+      var displayNameValidate = newDisplayName.displayNameValidator;
+      if (displayNameValidate != null) {
+        dismissLoadingBar();
+        showToast(displayNameValidate);
+        return;
+      }
+      var response =
+          await userManager.updateCurrentUserDisplayName(newDisplayName);
+      if (response) {
+        oldDisplayName = newDisplayName;
+      }
+    }
+
+    if (isDescriptionChanged()) {
+      var response =
+          await userManager.updateCurrentUserDescription(newDescription);
+      if (response) {
+        oldDescription = newDescription;
+      }
+    }
+
+    if (isWebsiteChanged()) {
+      if (!(newWebsite.urlControl)) {
+        showToast("Url type is not correct");
+        return;
+      }
+      var response = await userManager.updateCurrentUserWebsite(newWebsite);
+      if (response) {
+        oldWebsite = newWebsite;
+      }
+    }
+
+    if (isBirthDateChanged()) {
+      if (newBirthDate != null) {
+        var response =
+            await userManager.updateCurrentUserBirthDate(newBirthDate);
+        if (response) {
+          oldBirthDate = newBirthDate;
+        }
+      }
+    }
+
+    dismissLoadingBar();
+    profileViewModel.refreshPage();
+    context!.pop;
+  }
+
+  void startLoadingBar() => loadingPBar.show(context!);
+  void dismissLoadingBar() => loadingPBar.dismiss();
+
+  bool isProfilePhotoChanged() => ppImageFile == null ? false : true;
+
+  bool isDisplayNameChanged() =>
+      oldDisplayName == displayNameController.text ? false : true;
+
+  bool isDescriptionChanged() =>
+      oldDescription == descriptionController.text ? false : true;
+
+  bool isWebsiteChanged() =>
+      oldWebsite == websiteController.text ? false : true;
+
+  bool isBirthDateChanged() => oldBirthDate == birthDate ? false : true;
+
+  void showDatePicker() {
+    displayNameFocusNode.unfocus();
+    descriptionFocusNode.unfocus();
+    websiteFocusNode.unfocus();
+    showDatePickerSheet(
+      context: context!,
+      onDateTimeSelected: (date) => selectDateTime(date),
+    );
+  }
+
+  selectDateTime(DateTime date) {
+    var formattedDate = DateFormat("MMMM d, yyyy").format(date);
+    birthDate = Timestamp.fromDate(date);
+    birthDateController.text = formattedDate;
+  }
+}
---
+++ lib/pages/profile/view_model/edit_profile_view_model/edit_profile_view_model.g.dart
@@ -0,0 +1,47 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'edit_profile_view_model.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
+
+mixin _$EditProfileViewModel on _EditProfileViewModelBase, Store {
+  final _$ppImageFileAtom = Atom(name: '_EditProfileViewModelBase.ppImageFile');
+
+  @override
+  File? get ppImageFile {
+    _$ppImageFileAtom.reportRead();
+    return super.ppImageFile;
+  }
+
+  @override
+  set ppImageFile(File? value) {
+    _$ppImageFileAtom.reportWrite(value, super.ppImageFile, () {
+      super.ppImageFile = value;
+    });
+  }
+
+  final _$_EditProfileViewModelBaseActionController =
+      ActionController(name: '_EditProfileViewModelBase');
+
+  @override
+  dynamic changePpImageFile(File? file) {
+    final _$actionInfo = _$_EditProfileViewModelBaseActionController
+        .startAction(name: '_EditProfileViewModelBase.changePpImageFile');
+    try {
+      return super.changePpImageFile(file);
+    } finally {
+      _$_EditProfileViewModelBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  String toString() {
+    return '''
+ppImageFile: ${ppImageFile}
+    ''';
+  }
+}
