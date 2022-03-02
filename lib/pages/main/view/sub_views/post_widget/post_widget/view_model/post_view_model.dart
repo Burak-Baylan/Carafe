9424c2d350862adffa5a0233d9cd50ef49af4ad9
Burak-Baylan <dsjkadnas@gmail.com>
2022-03-02 20:27:52 +0300

    update: post vm. (optimized: save post, delete post, navigate | add: post status informations)

--- lib/pages/main/view/sub_views/post_widget/post_widget/view_model/post_view_model.dart
+++ lib/pages/main/view/sub_views/post_widget/post_widget/view_model/post_view_model.dart
@@ -1,16 +1,14 @@
+import 'package:Carafe/pages/authenticate/model/user_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
 import '../../../../../../../core/base/view_model/base_view_model.dart';
-import '../../../../../../../core/extensions/color_extensions.dart';
-import '../../../../../../../core/helpers/status_bar_helper.dart';
+import '../../../../../../../core/firebase/firestore/manager/post_manager/post_manager.dart';
 import '../../../../../model/post_model.dart';
 import '../../../../../model/post_save_model.dart';
-import '../../../../add_post/view/add_post_page.dart';
-import '../../../../home/view/sub_views/home_page_full_screen_image/home_page_full_screen_image.dart';
 import '../../../../home/view_model/home_view_model.dart';
-import '../../full_screen_post_view/full_screen_post_view.dart';
-
+import 'helpers/post_status_informations_manager.dart';
+import 'helpers/post_view_model_navigators.dart';
 part 'post_view_model.g.dart';
 
 class PostViewModel = _PostViewModelBase with _$PostViewModel;
@@ -19,33 +17,33 @@ abstract class _PostViewModelBase extends BaseViewModel with Store {
   @override
   BuildContext? context;
   @override
-  setContext(BuildContext context) => this.context = context;
+  void setContext(BuildContext context) {
+    this.context = context;
+    postManager = firebasePostManager;
+  }
+
   setPostModel(PostModel postModel) => this.postModel = postModel;
-  setHomeViewModel(HomeViewModel homeViewModel) =>
-      this.homeViewModel = homeViewModel;
   @observable
-  bool moreCommentsLoadingProgressState = false;
-  @action
-  changeMoreCommentsLoadingProgressState() =>
-      moreCommentsLoadingProgressState = !moreCommentsLoadingProgressState;
-  @observable
-  ScrollPhysics? commentsScrollable;
-  @action
-  changeCommentsScrollable(ScrollPhysics? physics) =>
-      commentsScrollable = physics;
-  @action
-  lockScrollable() =>
-      changeCommentsScrollable(const NeverScrollableScrollPhysics());
-  @action
-  openScrollable() => changeCommentsScrollable(null);
+  UserModel? userModel;
   @observable
   IconData likeIcon = Icons.favorite_rounded;
   @observable
   IconData postSaveIcon = Icons.bookmark_outline;
   @observable
   List<PostModel> comments = [];
-  bool canMoreCommentsUpload = true;
-  int get commentsLength => comments.length;
+  @observable
+  bool isPostDeleted = false;
+  @observable
+  ScrollPhysics? commentsScrollable;
+  @action
+  void changeCommentsScrollable(ScrollPhysics? physics) =>
+      commentsScrollable = physics;
+  @action
+  void lockScrollable() =>
+      changeCommentsScrollable(const NeverScrollableScrollPhysics());
+  @action
+  void openScrollable() => changeCommentsScrollable(null);
+  bool canUploadMoreComments = true;
   late PostModel postModel;
   bool likeLock = false;
   bool saveLock = false;
@@ -53,15 +51,40 @@ abstract class _PostViewModelBase extends BaseViewModel with Store {
   IconData likedIcon = Icons.favorite_outlined;
   IconData unsavedIcon = Icons.bookmark_outline;
   IconData savedIcon = Icons.bookmark_outlined;
-  late HomeViewModel homeViewModel;
-  late DocumentReference sharedPostRef;
+  late DocumentReference currentPostRef;
+  late FirebasePostManager postManager;
+  PostViewModelNavigators postViewModelNavigators = PostViewModelNavigators();
+  var postStatusInformationsManager = PostStatusInformationsManager();
 
-  changeLikeLockState() => likeLock = !likeLock;
+  void changeLikeLockState() => likeLock = !likeLock;
+  void changePostSaveLockState() => saveLock = !saveLock;
 
-  changePostSaveLockState() => saveLock = !saveLock;
+  Query<Map<String, dynamic>> get commentsRef => currentPostRef
+      .collection(firebaseConstants.postCommentsText)
+      .orderBy(firebaseConstants.createdAtText, descending: true)
+      .where(firebaseConstants.isPostDeletedText, isEqualTo: false)
+      .limit(firebaseConstants.numberOfCommentsToBeReceiveAtOnce);
+
+  Query<Map<String, dynamic>> postCommentsRawRef() =>
+      firebaseConstants.aPostCommentsRef(postModel.postPath).where(
+            firebaseConstants.isPostDeletedText,
+            isEqualTo: false,
+          );
+
+  CollectionReference<Map<String, dynamic>> postLikesRef() =>
+      firebaseConstants.aPostLikesRef(postModel.postPath);
+
+  Future<void> initializeValues() async {
+    currentPostRef = firestore.doc(postModel.postPath);
+    await findPostOwnerUser();
+  }
 
   @action
-  findLikeIcon() async {
+  Future<void> findPostOwnerUser() async =>
+      userModel = await firebaseManager.getAUserInformation(postModel.authorId);
+
+  @action
+  Future<void> findLikeIcon() async {
     if (!(await getUserLikeState)) {
       likeIcon = likedIcon;
     } else {
@@ -70,7 +93,7 @@ abstract class _PostViewModelBase extends BaseViewModel with Store {
   }
 
   @action
-  findPostSaveIcon() async {
+  Future<void> findPostSaveIcon() async {
     if (!(await getUserPostSaveState)) {
       postSaveIcon = savedIcon;
     } else {
@@ -79,7 +102,7 @@ abstract class _PostViewModelBase extends BaseViewModel with Store {
   }
 
   Future<bool> get getUserLikeState async =>
-      await postManager.userLikeState(sharedPostRef, authService.userId!);
+      await postManager.userLikeState(currentPostRef, authService.userId!);
 
   Future<bool> get getUserPostSaveState async => await postManager
       .userPostSaveState(postModel.postId, authService.userId!);
@@ -89,12 +112,15 @@ abstract class _PostViewModelBase extends BaseViewModel with Store {
     if (likeLock) return;
     changeLikeLockState();
     if (await getUserLikeState) {
-      bool isPostLiked = await postManager.likePost(sharedPostRef, currentTime);
-      if (isPostLiked) likeIcon = likedIcon;
+      likeIcon = likedIcon;
+      bool isPostLiked = await postManager.likePost(
+          currentPostRef, currentTime, postModel.postId);
+      if (!isPostLiked) likeIcon = unlikedIcon;
     } else {
-      bool isPostUnliked =
-          await postManager.unlikePost(sharedPostRef, authService.userId!);
-      if (isPostUnliked) likeIcon = unlikedIcon;
+      likeIcon = unlikedIcon;
+      bool isPostUnliked = await postManager.unlikePost(
+          currentPostRef, authService.userId!, postModel.postId);
+      if (!isPostUnliked) likeIcon = likedIcon;
     }
     changeLikeLockState();
   }
@@ -107,90 +133,48 @@ abstract class _PostViewModelBase extends BaseViewModel with Store {
       savedAt: currentTime,
       userId: authService.userId!,
       postId: postModel.postId,
-      sharedPostRef: sharedPostRef.path,
+      sharedPostRef: currentPostRef.path,
     );
     if (await getUserPostSaveState) {
-      await postManager.savePost(model);
       postSaveIcon = savedIcon;
+      var response = await postManager.savePost(model);
+      if (!response) postSaveIcon = unsavedIcon;
     } else {
-      await postManager.unsavePost(model);
       postSaveIcon = unsavedIcon;
+      var response = await postManager.unsavePost(model);
+      if (!response) postSaveIcon = savedIcon;
     }
     changePostSaveLockState();
   }
 
-  void onPressedImage(List<ImageProvider<Object>?> imageProviders,
-      List<dynamic> imageUrls, int imageIndex) {
-    if (imageProviders[imageUrls.length - 1] == null) return;
-    StatusBarHelper.edgeToEdgeScreen();
-    Colors.transparent.changeBottomNavBarColor;
-    customNavigateToPage(
-      page: HomePageFullScreenImage(
-        imageProviders: imageProviders,
-        imageUrls: imageUrls,
-        imageIndex: imageIndex,
-        imagesDominantColor: postModel.imagesDominantColors,
-      ),
-      animate: false,
-    );
+  @action
+  Future delete(bool isAComment) async {
+    var deleteResponse = await postManager.delete(currentPostRef);
+    if (deleteResponse.errorMessage != null) {
+      showPostNotDeletedAlert();
+      return;
+    }
+    isPostDeleted = true;
+    showToast("Post deleted");
   }
 
-  void navigateToFullScreenPostView(PostViewModel viewModel) =>
-      customNavigateToPage(
-        page: FullScreenPostView(
-          postViewModel: viewModel,
-          postModel: postModel,
-          homeViewModel: homeViewModel,
-          postRef: sharedPostRef,
-        ),
-        animate: true,
+  void showPostNotDeletedAlert() => showAlert(
+        'Error Message1',
+        'Post could not be deleted. Please try again.',
+        context: context!,
       );
 
-  void navigateToReplyScreen({
-    PostModel? postModel,
-    DocumentReference<Object?>? postAddingRef,
-  }) =>
-      customNavigateToPage(
-        page: AddPostPage(
-          isAComment: true,
-          postAddingReference: (postAddingRef ?? sharedPostRef)
-              .collection(firebaseConstants.postCommentsText),
-          replyingPostPostModel: postModel ?? this.postModel,
-        ),
-        animate: true,
-      );
-
-  DocumentReference findACommentPathFromComments(int index) => sharedPostRef
+  DocumentReference findACommentPathFromComments(int index) => currentPostRef
       .collection(firebaseConstants.postCommentsText)
       .doc(comments[index].postId);
 
-  void findCommentsPath(DocumentReference<Object?>? postRef) {
-    if (postRef == null) {
-      sharedPostRef =
-          firebaseConstants.allPostsCollectionRef.doc(postModel.postId);
-    } else {
-      sharedPostRef = postRef;
-    }
-  }
-
-  @action
-  Future<List<PostModel>> getComments() async {
-    openLoadMoreCommentsState();
-    lockScrollable();
-    comments.clear();
-    var commentsData = await firebasePostManger.getComments(
-        sharedPostRef.collection(firebaseConstants.postCommentsText));
-    if (commentsData.error == null) {
-      comments = commentsData.data!;
-    }
-    openScrollable();
-    return comments;
-  }
+  void findCommentsPath(String postRef) =>
+      currentPostRef = firestore.doc(postRef);
 
   ScrollController get scrollController {
     var controller = ScrollController();
     controller.addListener(() async {
-      if (!canMoreCommentsUpload || moreCommentsLoadingProgressState) return;
+      if (!canUploadMoreComments) return;
       if (controller.offset >= controller.position.maxScrollExtent) {
         lockScrollable();
         await loadMoreComments();
@@ -201,28 +185,74 @@ abstract class _PostViewModelBase extends BaseViewModel with Store {
   }
 
   @action
-  Future<void> loadMoreComments() async {
-    if (!canMoreCommentsUpload || moreCommentsLoadingProgressState) return;
-    changeMoreCommentsLoadingProgressState();
+  Future<List<PostModel>> getComments() async {
+    openLoadMoreCommentsState();
     lockScrollable();
-    var commentData = await firebasePostManger.loadMoreComment(sharedPostRef);
-    if (commentData.error == null) {
-      loadMoreCommentsLockControl(commentData.data!.length);
-      for (var comment in commentData.data!) {
+    comments.clear();
+    var commentsData = await postManager.getPosts(ref: commentsRef);
+    if (commentsData.error == null) {
+      comments = commentsData.data!;
+    }
+    openScrollable();
+    return comments;
+  }
+
+  @action
+  Future<void> loadMoreComments() async {
+    if (!canUploadMoreComments) return;
+    lockScrollable();
+    var commentData = await postManager.loadMorePost(ref: commentsRef);
+    if (commentData.error == null && commentData.data != null) {
+      var loadedComments = commentData.data!;
+      loadMoreCommentsLockControl(loadedComments.length);
+      for (var comment in loadedComments) {
         comments.add(comment);
       }
     }
-    changeMoreCommentsLoadingProgressState();
     openScrollable();
     return;
   }
 
-  lockLoadMoreCommentsState() => canMoreCommentsUpload = false;
-  openLoadMoreCommentsState() => canMoreCommentsUpload = true;
+  void lockLoadMoreCommentsState() => canUploadMoreComments = false;
+  void openLoadMoreCommentsState() => canUploadMoreComments = true;
 
-  loadMoreCommentsLockControl(int size) {
-    if (size < firebaseConstants.numberOfCommentsToBeUploadedAtOnce) {
+  void loadMoreCommentsLockControl(int size) {
+    if (size < firebaseConstants.numberOfCommentsToBeReceiveAtOnce) {
       lockLoadMoreCommentsState();
     }
   }
+
+  Future<void> addToPostViews() async => await postStatusInformationsManager
+      .addToPostViews(currentPostRef, currentTime);
+
+  Future<void> addToPostClicked() async => await postStatusInformationsManager
+      .addToPostClicked(currentPostRef, currentTime);
+
+  Future<void> addToProfileVisits() async => await postStatusInformationsManager
+      .addToProfileVisits(currentPostRef, currentTime);
+
+  void onPressedImage(
+    List<ImageProvider<Object>?> imageProviders,
+    List<dynamic> imageUrls,
+    int imageIndex,
+    String imageTag,
+  ) =>
+      postViewModelNavigators.onPressedImage(
+          imageProviders, imageUrls, imageIndex, imageTag, postModel);
+
+  void navigateToPostStatus(PostViewModel postViewModel) =>
+      postViewModelNavigators.navigateToPostStatus(postViewModel, postModel);
+
+  void navigateToFullScreenPostView(PostViewModel viewModel) =>
+      postViewModelNavigators.navigateToFullScreenPostView(
+          viewModel, postModel, currentPostRef);
+
+  void navigateToReplyScreen({
+    PostModel? postModel,
+    DocumentReference<Object?>? postAddingRef,
+  }) =>
+      postViewModelNavigators.navigateToReplyScreen(
+        postModel ?? this.postModel,
+        postAddingRef ?? currentPostRef,
+      );
 }
--- lib/pages/main/view/sub_views/post_widget/post_widget/view_model/post_view_model.g.dart
+++ lib/pages/main/view/sub_views/post_widget/post_widget/view_model/post_view_model.g.dart
@@ -9,36 +9,18 @@ part of 'post_view_model.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
 
 mixin _$PostViewModel on _PostViewModelBase, Store {
-  final _$moreCommentsLoadingProgressStateAtom =
-      Atom(name: '_PostViewModelBase.moreCommentsLoadingProgressState');
+  final _$userModelAtom = Atom(name: '_PostViewModelBase.userModel');
 
   @override
-  bool get moreCommentsLoadingProgressState {
-    _$moreCommentsLoadingProgressStateAtom.reportRead();
-    return super.moreCommentsLoadingProgressState;
+  UserModel? get userModel {
+    _$userModelAtom.reportRead();
+    return super.userModel;
   }
 
   @override
-  set moreCommentsLoadingProgressState(bool value) {
-    _$moreCommentsLoadingProgressStateAtom
-        .reportWrite(value, super.moreCommentsLoadingProgressState, () {
-      super.moreCommentsLoadingProgressState = value;
-    });
-  }
-
-  final _$commentsScrollableAtom =
-      Atom(name: '_PostViewModelBase.commentsScrollable');
-
-  @override
-  ScrollPhysics? get commentsScrollable {
-    _$commentsScrollableAtom.reportRead();
-    return super.commentsScrollable;
-  }
-
-  @override
-  set commentsScrollable(ScrollPhysics? value) {
-    _$commentsScrollableAtom.reportWrite(value, super.commentsScrollable, () {
-      super.commentsScrollable = value;
+  set userModel(UserModel? value) {
+    _$userModelAtom.reportWrite(value, super.userModel, () {
+      super.userModel = value;
     });
   }
 
@@ -87,11 +69,50 @@ mixin _$PostViewModel on _PostViewModelBase, Store {
     });
   }
 
+  final _$isPostDeletedAtom = Atom(name: '_PostViewModelBase.isPostDeleted');
+
+  @override
+  bool get isPostDeleted {
+    _$isPostDeletedAtom.reportRead();
+    return super.isPostDeleted;
+  }
+
+  @override
+  set isPostDeleted(bool value) {
+    _$isPostDeletedAtom.reportWrite(value, super.isPostDeleted, () {
+      super.isPostDeleted = value;
+    });
+  }
+
+  final _$commentsScrollableAtom =
+      Atom(name: '_PostViewModelBase.commentsScrollable');
+
+  @override
+  ScrollPhysics? get commentsScrollable {
+    _$commentsScrollableAtom.reportRead();
+    return super.commentsScrollable;
+  }
+
+  @override
+  set commentsScrollable(ScrollPhysics? value) {
+    _$commentsScrollableAtom.reportWrite(value, super.commentsScrollable, () {
+      super.commentsScrollable = value;
+    });
+  }
+
+  final _$findPostOwnerUserAsyncAction =
+      AsyncAction('_PostViewModelBase.findPostOwnerUser');
+
+  @override
+  Future<void> findPostOwnerUser() {
+    return _$findPostOwnerUserAsyncAction.run(() => super.findPostOwnerUser());
+  }
+
   final _$findLikeIconAsyncAction =
       AsyncAction('_PostViewModelBase.findLikeIcon');
 
   @override
-  Future findLikeIcon() {
+  Future<void> findLikeIcon() {
     return _$findLikeIconAsyncAction.run(() => super.findLikeIcon());
   }
 
@@ -99,7 +120,7 @@ mixin _$PostViewModel on _PostViewModelBase, Store {
       AsyncAction('_PostViewModelBase.findPostSaveIcon');
 
   @override
-  Future findPostSaveIcon() {
+  Future<void> findPostSaveIcon() {
     return _$findPostSaveIconAsyncAction.run(() => super.findPostSaveIcon());
   }
 
@@ -115,6 +136,13 @@ mixin _$PostViewModel on _PostViewModelBase, Store {
   @override
   Future<dynamic> save() {
     return _$saveAsyncAction.run(() => super.save());
+  }
+
+  final _$deleteAsyncAction = AsyncAction('_PostViewModelBase.delete');
+
+  @override
+  Future<dynamic> delete(bool isAComment) {
+    return _$deleteAsyncAction.run(() => super.delete(isAComment));
   }
 
   final _$getCommentsAsyncAction =
@@ -137,18 +165,7 @@ mixin _$PostViewModel on _PostViewModelBase, Store {
       ActionController(name: '_PostViewModelBase');
 
   @override
-  dynamic changeMoreCommentsLoadingProgressState() {
-    final _$actionInfo = _$_PostViewModelBaseActionController.startAction(
-        name: '_PostViewModelBase.changeMoreCommentsLoadingProgressState');
-    try {
-      return super.changeMoreCommentsLoadingProgressState();
-    } finally {
-      _$_PostViewModelBaseActionController.endAction(_$actionInfo);
-    }
-  }
-
-  @override
-  dynamic changeCommentsScrollable(ScrollPhysics? physics) {
+  void changeCommentsScrollable(ScrollPhysics? physics) {
     final _$actionInfo = _$_PostViewModelBaseActionController.startAction(
         name: '_PostViewModelBase.changeCommentsScrollable');
     try {
@@ -159,7 +176,7 @@ mixin _$PostViewModel on _PostViewModelBase, Store {
   }
 
   @override
-  dynamic lockScrollable() {
+  void lockScrollable() {
     final _$actionInfo = _$_PostViewModelBaseActionController.startAction(
         name: '_PostViewModelBase.lockScrollable');
     try {
@@ -170,7 +187,7 @@ mixin _$PostViewModel on _PostViewModelBase, Store {
   }
 
   @override
-  dynamic openScrollable() {
+  void openScrollable() {
     final _$actionInfo = _$_PostViewModelBaseActionController.startAction(
         name: '_PostViewModelBase.openScrollable');
     try {
@@ -183,11 +200,12 @@ mixin _$PostViewModel on _PostViewModelBase, Store {
   @override
   String toString() {
     return '''
-moreCommentsLoadingProgressState: ${moreCommentsLoadingProgressState},
-commentsScrollable: ${commentsScrollable},
+userModel: ${userModel},
 likeIcon: ${likeIcon},
 postSaveIcon: ${postSaveIcon},
-comments: ${comments}
+comments: ${comments},
+isPostDeleted: ${isPostDeleted},
+commentsScrollable: ${commentsScrollable}
     ''';
   }
 }
