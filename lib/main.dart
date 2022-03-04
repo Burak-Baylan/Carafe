b100e1b9f3df5aead2808226457c34b401567a30
Burak-Baylan <dsjkadnas@gmail.com>
2022-03-04 11:33:45 +0300

    update: main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,26 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
-
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'app/app_settings/app_settings_view_model.dart';
-import 'core/extensions/color_extensions.dart';
-import 'core/extensions/context_extensions.dart';
+import 'core/helpers/system_navigation_bar_helper.dart';
+import 'core/hive/hive_helper.dart';
 import 'core/init/navigation/route/navigation_route.dart';
 import 'core/init/navigation/service/navigation_service.dart';
-import 'pages/authenticate/authenticate_view.dart';
-import 'pages/main/view/main_screen.dart';
+import 'pages/main/view_model/main_view_view_model.dart';
 
-void main() {
+MainViewViewModel mainVm = MainViewViewModel();
+
+main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.removeAfter(start);
   runApp(MyApp());
+}
+
+Future start(BuildContext context) async {
+  await HiveHelper.instance.initHive();
+  await Firebase.initializeApp();
+  await mainVm.startApp();
 }
 
 class MyApp extends StatelessWidget {
@@ -21,51 +29,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Colors.transparent.changeStatusBarColor;
+    SystemBottomNavigationBarHelper.lightBottomNavBar;
     return Observer(builder: (_) {
       return MaterialApp(
         navigatorKey: NavigationService.instance.navigatorKey,
         onGenerateRoute: NavigationRoute.instance.generateRoute,
         debugShowCheckedModeBanner: false,
         theme: viewModel.appTheme,
-        home: FutureBuilder(
-          future: start(),
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              return Center(
-                //errorScreen
-                child: Text("ERROR: " + snapshot.error.toString()),
-              );
-            }
-            if (snapshot.connectionState == ConnectionState.done) {
-              snapshot.data;
-              FirebaseAuth auth = FirebaseAuth.instance;
-              return autoLoginControl(auth);
-            }
-            return Container(
-              //splashScreen
-              color: Colors.white,
-              width: context.width,
-              height: context.height,
-              child: const Center(
-                child: CircularProgressIndicator(),
-              ),
-            );
-          },
-        ),
+        home: mainVm.startingPage,
       );
     });
-  }
-
-  Future<void>? start() async {
-    await Firebase.initializeApp();
-  }
-
-  Widget autoLoginControl(FirebaseAuth auth) {
-    if (auth.currentUser != null) {
-      return const MainScreen();
-    }
-    return AuthenticateView();
-
   }
 }
