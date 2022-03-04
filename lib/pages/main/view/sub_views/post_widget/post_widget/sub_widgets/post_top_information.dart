5e9b85bbd52dd048ecf802609416a7dcd6bcc5b8
Burak-Baylan <dsjkadnas@gmail.com>
2022-03-04 11:04:39 +0300

    update: post top information (navigate category view when clicked)

--- lib/pages/main/view/sub_views/post_widget/post_widget/sub_widgets/post_top_information.dart
+++ lib/pages/main/view/sub_views/post_widget/post_widget/sub_widgets/post_top_information.dart
@@ -1,3 +1,5 @@
+import 'package:Carafe/main.dart';
+import 'package:Carafe/pages/category/view/category_view.dart';
 import 'package:flutter/material.dart';
 import '../../../../../../../app/constants/app_constants.dart';
 import '../../../../../../../core/extensions/context_extensions.dart';
@@ -22,75 +24,93 @@ class PostTopInformation extends StatelessWidget {
   }
 
   IconData? categoryIcon;
-  String? categoryName;
+  String? longCategoryName;
+  String? shortCategoryName;
 
-  _findCategory() {
+  void _findCategory() {
     if (isPostPinned) {
-      categoryName = PostContstants.PINNED_POST;
+      longCategoryName = PostContstants.PINNED_POST;
       categoryIcon = PostContstants.PINNED_POST_ICON;
       return;
     }
     switch (model.category) {
       case PostContstants.ALL:
-        categoryName = null;
+        longCategoryName = null;
+        shortCategoryName = null;
         categoryIcon = null;
         break;
       case PostContstants.TECHNOLOGY:
-        categoryName = 'Category: ' + PostContstants.TECHNOLOGY;
+        longCategoryName = 'Category: ' + PostContstants.TECHNOLOGY;
+        shortCategoryName = PostContstants.TECHNOLOGY;
         categoryIcon = PostContstants.TECHNOLOGY_ICON;
         break;
       case PostContstants.SOFTWARE:
-        categoryName = 'Category: ' + PostContstants.SOFTWARE;
+        longCategoryName = 'Category: ' + PostContstants.SOFTWARE;
+        shortCategoryName = PostContstants.SOFTWARE;
         categoryIcon = PostContstants.SOFTWARE_ICON;
         break;
       case PostContstants.GAMES:
-        categoryName = 'Category: ' + PostContstants.GAMES;
+        longCategoryName = 'Category: ' + PostContstants.GAMES;
+        shortCategoryName = PostContstants.GAMES;
         categoryIcon = PostContstants.GAMES_ICON;
         break;
       case PostContstants.ADVICES:
-        categoryName = 'Category: ' + PostContstants.ADVICES;
+        longCategoryName = 'Category: ' + PostContstants.ADVICES;
+        shortCategoryName = PostContstants.ADVICES;
         categoryIcon = PostContstants.ADVICES_ICON;
         break;
       case PostContstants.ENTERPRISE:
-        categoryName = 'Category: ' + PostContstants.ENTERPRISE;
+        longCategoryName = 'Category: ' + PostContstants.ENTERPRISE;
+        shortCategoryName = PostContstants.ENTERPRISE;
         categoryIcon = PostContstants.ENTERPRISE_ICON;
         break;
       default:
     }
   }
 
-  get _buildCategoryWidget =>
-      categoryName == null ? _categoryWidget(true) : _categoryWidget(false);
+  Widget get _buildCategoryWidget =>
+      longCategoryName == null ? _categoryWidget(true) : _categoryWidget(false);
 
   Widget _categoryWidget(bool categoryNameIsNull) => SizedBox(
         height: categoryNameIsNull ? 0 : null,
         child: Row(
           children: [
-            SizedBox(
-              width: context.width / 7.5,
-              child: Align(
-                child: categoryNameIsNull
-                    ? null
-                    : Icon(categoryIcon,
-                        size: context.width / 25, color: AppColors.secondary),
-                alignment: Alignment.centerRight,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 10),
-              child: InkWell(
-                onTap: () {},
-                child: Text(
-                  categoryNameIsNull ? "" : categoryName!,
-                  style: TextStyle(
-                    color: AppColors.secondary,
-                    fontSize: context.width / 30,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ),
+            buildCategoryIcon(categoryNameIsNull),
+            buildCategoryText(categoryNameIsNull),
           ],
         ),
       );
+
+  Widget buildCategoryIcon(bool categoryNameIsNull) => SizedBox(
+        width: context.width / 7.5,
+        child: Align(
+          child: categoryNameIsNull
+              ? null
+              : Icon(categoryIcon,
+                  size: context.width / 25, color: AppColors.secondary),
+          alignment: Alignment.centerRight,
+        ),
+      );
+
+  Widget buildCategoryText(bool categoryNameIsNull) => Padding(
+        padding: const EdgeInsets.only(left: 10),
+        child: InkWell(
+          onTap: () => navigateToCategoryPage(),
+          child: Text(
+            categoryNameIsNull ? "" : longCategoryName!,
+            style: TextStyle(
+              color: AppColors.secondary,
+              fontSize: context.width / 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      );
+
+  void navigateToCategoryPage() {
+    mainVm.customNavigateToPage(
+      page: CategoryView(categoryName: shortCategoryName!),
+      animate: true,
+    );
+  }
 }
