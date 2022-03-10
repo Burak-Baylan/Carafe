85d846622840aea733217f6bc8292dacc273d777
Burak-Baylan <dsjkadnas@gmail.com>
2022-03-10 14:06:18 +0300

    update: liked and saved posts getter organized

--- lib/core/firebase/firestore/manager/post_manager/liked_and_saved_posts_getter.dart
+++ lib/core/firebase/firestore/manager/post_manager/liked_and_saved_posts_getter.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-
 import '../../../../../pages/main/model/like_model.dart';
 import '../../../../../pages/main/model/post_model.dart';
 import '../../../../../pages/main/model/post_save_model.dart';
@@ -12,44 +11,68 @@ class LikedAndSavedPostsGetter extends FirebaseBase {
   late Query<Map<String, dynamic>> userLikedPostsRef;
   late Query<Map<String, dynamic>> userSavedPostsRef;
 
+  Future<List<PostModel>?> getPosts({
+    required Query<Map<String, dynamic>> reference,
+    required ListingType listingType,
+  }) async {
+    var rawLikedPosts = await firestoreService.getQuery(reference);
+    if (rawLikedPosts.error != null || rawLikedPosts.data == null) {
+      return null;
+    }
+    return await getPostsList(rawLikedPosts.data!.docs, listingType);
+  }
+
+  Future<List<PostModel>?> loadMorePosts({
+    required Query<Map<String, dynamic>> reference,
+    required QueryDocumentSnapshot<Map<String, dynamic>> lastVisiblePost,
+    required ListingType listingType,
+  }) async {
+    var rawRef = reference.startAfterDocument(lastVisiblePost);
+    var rawLikedPosts = await firestoreService.getQuery(rawRef);
+    if (rawLikedPosts.error != null || rawLikedPosts.data == null) {
+      return null;
+    }
+    return await getPostsList(rawLikedPosts.data!.docs, listingType);
+  }
+
   Future<List<PostModel>?> getLikedPosts(
     Query<Map<String, dynamic>> userLikedPostsRef,
   ) async {
     this.userLikedPostsRef = userLikedPostsRef;
-    var rawLikedPosts = await firestoreService.getQuery(userLikedPostsRef);
-    if (rawLikedPosts.error != null || rawLikedPosts.data == null) {
-      return null;
-    }
-    return await getPostsList(rawLikedPosts.data!.docs, ListingType.likes);
+    var likedPosts = await getPosts(
+      reference: userLikedPostsRef,
+      listingType: ListingType.likes,
+    );
+    return likedPosts;
   }
 
   Future<List<PostModel>?> getMoreLikedPosts() async {
-    var rawLikedPosts = await firestoreService
-        .getQuery(userLikedPostsRef.startAfterDocument(lastVisibleLikedPost));
-    if (rawLikedPosts.error != null || rawLikedPosts.data == null) {
-      return null;
-    }
-    return await getPostsList(rawLikedPosts.data!.docs, ListingType.likes);
+    var posts = await loadMorePosts(
+      reference: userLikedPostsRef,
+      lastVisiblePost: lastVisibleLikedPost,
+      listingType: ListingType.likes,
+    );
+    return posts;
   }
 
   Future<List<PostModel>?> getSavedPosts(
     Query<Map<String, dynamic>> userSavedPostsRef,
   ) async {
     this.userSavedPostsRef = userSavedPostsRef;
-    var rawSavedPosts = await firestoreService.getQuery(userSavedPostsRef);
-    if (rawSavedPosts.error != null || rawSavedPosts.data == null) {
-      return null;
-    }
-    return await getPostsList(rawSavedPosts.data!.docs, ListingType.savedPosts);
+    var likedPosts = await getPosts(
+      reference: userSavedPostsRef,
+      listingType: ListingType.savedPosts,
+    );
+    return likedPosts;
   }
 
   Future<List<PostModel>?> getMoreSavedPosts() async {
-    var rawSavedPosts = await firestoreService
-        .getQuery(userSavedPostsRef.startAfterDocument(lastVisibleSavedPost));
-    if (rawSavedPosts.error != null || rawSavedPosts.data == null) {
-      return null;
-    }
-    return await getPostsList(rawSavedPosts.data!.docs, ListingType.savedPosts);
+    var posts = await loadMorePosts(
+      reference: userSavedPostsRef,
+      lastVisiblePost: lastVisibleSavedPost,
+      listingType: ListingType.savedPosts,
+    );
+    return posts;
   }
 
   Future<List<PostModel>?> getPostsList(
