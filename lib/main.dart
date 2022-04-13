fcdc170c81f7c4bc18a38fa0eed283ca04bc8f11
Burak-Baylan <dsjkadnas@gmail.com>
2022-04-13 05:04:10 +0300

    update: landscape mode disabled

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:overlay_support/overlay_support.dart';
@@ -32,6 +33,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SystemBottomNavigationBarHelper.lightBottomNavBar;
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return Observer(builder: (_) {
       return OverlaySupport(
         child: MaterialApp(
