e8c5e142bc4b6d5fafb591a5d1e2bb62ee523e32
Burak-Baylan <dsjkadnas@gmail.com>
2022-04-13 05:07:07 +0300

    update: add new svg consts, status bar helper

--- lib/core/constants/svg/svg_constants.dart
+++ lib/core/constants/svg/svg_constants.dart
@@ -7,4 +7,7 @@ class SVGConstants {
   static String get sad => 'sad'.toSvg;
   static String get nothingHere => 'nothing_here'.toSvg;
   static String get noFollowing => 'no_following'.toSvg;
+  static String get authentication => 'authentication'.toSvg;
+  static String get post => 'post'.toSvg;
+  static String get search => 'search'.toSvg;
 }
--- lib/core/helpers/status_bar_helper.dart
+++ lib/core/helpers/status_bar_helper.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/services.dart';
 
 class StatusBarHelper {
-  static open() => SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
-      overlays: SystemUiOverlay.values);
+  static open() => SystemChrome.setEnabledSystemUIMode(
+        SystemUiMode.manual,
+        overlays: SystemUiOverlay.values,
+      );
 
   static close() =>
       SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
 
   static edgeToEdgeScreen() =>
       SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+
+  static defaultScreen() =>
+      SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
 }
